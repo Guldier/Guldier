18163b5e3e42435c792a2fd5682f2623efee7419
Damian Jadacki <damianjadacki@gmail.com>
2020-09-12 08:42:37 +0200

    admin_change

--- shop/admin.py
+++ shop/admin.py
@@ -2,10 +2,7 @@ from django.contrib import admin
 from .models import Dish, AddOn, Composition, Cart, Orders, WeekDish
 
 class WeekDishAdmin(admin.ModelAdmin):
-    list_display = ('day', 'name')
-    search_fields = ('day', 'name')
-    list_filter = ('day')
-
+    search_fields = ['day']
 
 admin.site.register(Dish)
 admin.site.register(AddOn)
