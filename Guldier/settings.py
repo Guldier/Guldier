3d514e9e4836dced685621e5295f2716fedaeaf3
Pawel <pawel.lisek.fp@gmail.com>
2021-12-05 23:20:01 +0100

    tweaks in Guildier/settings.py

--- Guldier/settings.py
+++ Guldier/settings.py
@@ -20,7 +20,6 @@ with open('/etc/config.json') as config_file:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -30,8 +29,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.17.128', '127.0.0.1']
-
+ALLOWED_HOSTS = ['192.168.17.128', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -95,7 +93,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -114,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -127,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -147,3 +142,4 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STRIPE_PUBLIC_KEY = config.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config.get('STRIPE_PRIVATE_KEY')
+STRIPE_WEBHOOK_SECRET = config.get('STRIPE_WEBHOOK_SECRET')
