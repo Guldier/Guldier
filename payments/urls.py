8c857f1270943d57ca403e9f7872f0b899310151
Pawel <pawel.lisek.fp@gmail.com>
2021-12-05 23:10:09 +0100

    successful top-up of a user account

--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateCheckoutSessionView, SuccessView, CancelView, ProductLandingPageView
+from .views import CreateCheckoutSessionView, SuccessView, CancelView, ProductLandingPageView, stripe_webhook
 
 app_name = 'payments'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('success/', SuccessView.as_view(), name='success'),
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('top-up/', ProductLandingPageView.as_view(), name='top_up'),
+    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 from django.views import View
-from django.http import JsonResponse
+from django.http import HttpResponse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
-from django.contrib.auth.models import User
+from users.models import Profile
 
 import stripe
-from .models import Price, TopUp
-
+from rest_framework.utils import json
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -16,13 +17,11 @@ class ProductLandingPageView(TemplateView):
     template_name = 'top_up.html'
 
     def get_context_data(self, **kwargs):
-
         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
         context.update({
             "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY,
 
             "product": 'Doładowanie punktów',
-            # "price": price,
         })
         return context
 
@@ -30,19 +29,14 @@ class ProductLandingPageView(TemplateView):
 class CreateCheckoutSessionView(View):
 
     def post(self, request, *args, **kwargs):
-
         topup_value = request.POST.get('topup')
         intent_value = int(topup_value) * 100
-
-        TopUp.objects.create(amount_intent_payment=topup_value, user=request.user)
-
         YOUR_DOMAIN = 'http://127.0.0.1:8000'
+        user = request.user
 
         checkout_session = stripe.checkout.Session.create(
-            line_items = [
+            line_items=[
                 {
-                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                    # 'price': price.stripe_id,
                     'price_data': {
                         'currency': 'usd',
                         'unit_amount': intent_value,
@@ -53,20 +47,56 @@ class CreateCheckoutSessionView(View):
                     'quantity': 1,
                 },
             ],
-
-            mode = 'payment',
-            success_url = YOUR_DOMAIN + '/success',
-            cancel_url = YOUR_DOMAIN + '/cancel',
+            metadata={
+                "user_profile_id": user.profile.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/profile',
+            cancel_url=YOUR_DOMAIN + '/payments/cancel',
         )
 
-        return redirect(checkout_session.url, code=303)
+        return redirect(checkout_session.url, code=303, context={'message': 'Your account has been topped up.'})
 
 
 class SuccessView(TemplateView):
-    template_name = 'succcess.html'
+    template_name = 'success.html'
 
 
 class CancelView(TemplateView):
     template_name = 'cancel.html'
 
 
+@csrf_exempt
+def stripe_webhook(request, customer_email=None):
+    payload = request.body
+    # header in the response that is coming from Stripe
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), sig_header, stripe.api_key
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event.type == 'checkout.session.completed':
+        completed_checkout_session = event.data.object
+        amount_received = int(completed_checkout_session.amount_total / 100)
+        user_profile_id = completed_checkout_session.metadata.user_profile_id
+        profile = Profile.objects.get(pk=user_profile_id)
+        profile.money += amount_received
+        profile.save()
+
+        # send_mail(
+        #     subject='Your account has been topped up',
+        #     message='Thanks for your purchase',
+        #     recipient_list=[customer_email],
+        #     from_email='some_email@test.com',
+        # )
+    # # Passed signature verification
+    return HttpResponse(status=200)
